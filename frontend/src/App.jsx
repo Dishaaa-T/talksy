c87ecfac8ce12d7a2fdcdee86e173ed64a31b270
Disha <smilesniyati@gmail.com>
2026-06-15 16:18:13 +0530

    added files and setup react notifications using react-toastify. Also added pages for the app and setup routing using react-router-dom.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,35 @@
+import {Route, Routes} from 'react-router-dom';
+
+import HomePage from "../pages/HomePage.jsx";
+import LoginPage from "../pages/LoginPage.jsx";
+import ChatPage from "../pages/ChatPage.jsx";
+import CallPage from "../pages/CallPage.jsx";
+import SignUpPage from "../pages/SignUpPage.jsx";
+import NotificationsPage from "../pages/NotificationsPage.jsx";
+import OnboardingPage from "../pages/OnboardingPage.jsx";
+
+import toast,{Toaster} from "react-hot-toast";
+
 const App = () => {
+  
   return (
-    <div className="bg-red-500 h-screen text-5xl" data-theme="night">
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
+    <div className="h-screen" data-theme="night">
+      
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/call" element={<CallPage />} />
+        <Route path="/notifications" element={<NotificationsPage />} />
+        <Route path="/onboarding" element={<OnboardingPage />} />
+        
+      </Routes>
+
+      
     </div>
   );
 };
 
 export default App;
+
